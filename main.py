48e67f7cd7604d7f4be3ebae31df5b62fa11d729
Maureen Cooper Rios <maureen.cooper@uc.cl>
2022-03-28 22:30:25 -0300

    feat(main): add function get_top_10_retweets

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ import pandas as pd
 
 def get_dataframe(json_path):
     return pd.read_json(json_path, lines=True)
+
+def get_top_10_retweets(dataframe):
+    _dataframe = dataframe.copy()
+    
+    return _dataframe.sort_values('retweetCount', ascending=False)['retweetCount'].head(10)
+
