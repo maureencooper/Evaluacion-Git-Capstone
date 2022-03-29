7732d92161f0c596509520a5934f082de2454939
Maureen Cooper Rios <maureen.cooper@uc.cl>
2022-03-28 22:47:18 -0300

    feat(main): add funtion get_top_10_active_users

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ def get_top_10_retweets(dataframe):
     
     return _dataframe.sort_values('retweetCount', ascending=False)['retweetCount'].head(10)
 
+def get_top_10_active_users(dataframe):
+    users = pd.json_normalize(dataframe['user'])
+    users = pd.DataFrame(users)
+
+    return users['id'].value_counts()[:10].sort_values(ascending=False)
+
+
+dataframe = get_dataframe('~/Desktop/data.json')
+get_top_10_active_users(dataframe)
